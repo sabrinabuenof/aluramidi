66391d6616e05be48c1e731397374981c2eefafa
Sabrina Bueno <bina_fbueno@hotmail.com>
2023-04-24 21:40:52 -0300

    for

--- main.js
+++ main.js
@@ -6,22 +6,13 @@ const listaDeTeclas = document.querySelectorAll(".tecla");
 
 let contador = 0;
 
-while (contador < listaDeTeclas.length) {
+// para (enquanto uma estrutura for verdadeira ela vai rodar)
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
   const tecla = listaDeTeclas[contador];
-
   const instrumento = tecla.classList[1];
-
-  //console.log(instrumento);
-
-  // templete string
   const idAudio = `#som_${instrumento}`;
-  //console.log(idAudio);
 
   tecla.onclick = function () {
     tocaSom(idAudio);
   };
-
-  contador = contador + 1;
-
-  //console.log(contador);
 }
